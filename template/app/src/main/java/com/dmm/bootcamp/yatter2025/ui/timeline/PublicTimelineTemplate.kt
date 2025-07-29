1c3de8e2f977d5091c1dcb48adaf3b7a8bc775cf
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2025-07-29 18:05:25 +0900

    タイムライン表示

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PublicTimelineTemplate.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PublicTimelineTemplate.kt
@@ -3,18 +3,25 @@ package com.dmm.bootcamp.yatter2025.ui.timeline
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dmm.bootcamp.yatter2025.R
 import com.dmm.bootcamp.yatter2025.ui.theme.Yatter2025Theme
 import com.dmm.bootcamp.yatter2025.ui.timeline.bindingmodel.YweetBindingModel
 
@@ -32,32 +39,53 @@ fun PublicTimelineTemplate(
     // PullToRefreshが実行されたときに、onRefreshの処理を発火
     val pullRefreshState = rememberPullRefreshState(isRefreshing, onRefresh)
 
-    // BoxコンポーザブルでLazyColumnをラップ
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .pullRefresh(pullRefreshState),//ユーザーが画面上部から下部に向けてスワイプしてPullToRefreshすることを検知するために、BoxコンポーザブルにpullRefresh(pullRefreshState)を追加します。
-        contentAlignment = Alignment.Center,
-    ){
-        LazyColumn (
-            // 画面全体に広がったままでは視認性が悪いため、contentPaddingを指定しLazyColumn内の要素にpaddingがつくようにしてみます
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(8.dp),
-        ){
-            items(yweetList){ item ->
-                YweetRow(yweetBindingModel = item)
-            }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+//                    Text(text = "タイムライン")
+                    Text(text = stringResource(R.string.timelin_top))
+                }
+            )
         }
 
-        // ローディング
-        // align(Alignment.TopCenter)指定をします。 こうすることにより、isRefreshingがtrueの間にローディングインディケータが表示され、falseになると非表示になります。
-        PullRefreshIndicator(
-            refreshing = isRefreshing,
-            state = pullRefreshState,
-            modifier = Modifier.align(Alignment.TopCenter)
-        )
-    }
+    ) { paddingValues ->
 
+        // BoxコンポーザブルでLazyColumnをラップ
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .pullRefresh(pullRefreshState) // ユーザーが画面上部から下部に向けてスワイプしてPullToRefreshすることを検知するために、BoxコンポーザブルにpullRefresh(pullRefreshState)を追加します。
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center,
+        ) {
+            LazyColumn(
+                // 画面全体に広がったままでは視認性が悪いため、contentPaddingを指定しLazyColumn内の要素にpaddingがつくようにしてみます
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(8.dp),
+            ) {
+                items(yweetList) { item ->
+                    YweetRow(yweetBindingModel = item)
+                }
+            }
+
+            // ローディング
+            // align(Alignment.TopCenter)指定をします。 こうすることにより、isRefreshingがtrueの間にローディングインディケータが表示され、falseになると非表示になります。
+            PullRefreshIndicator(
+                refreshing = isRefreshing,
+                state = pullRefreshState,
+                modifier = Modifier.align(Alignment.TopCenter)
+            )
+
+
+            // 初回読み込み
+            if (isLoading) {
+                CircularProgressIndicator()
+            }
+
+
+        }
+    }
 
 }
 
