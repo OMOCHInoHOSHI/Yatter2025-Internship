5a3592c1cbbca974760ffa774e234277c58d22bf
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2025-07-29 15:52:01 +0900

    表情のたびに画面更新する関数

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PublicTimelineViewModel.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PublicTimelineViewModel.kt
@@ -1,12 +1,14 @@
 package com.dmm.bootcamp.yatter2025.ui.timeline
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.dmm.bootcamp.yatter2025.domain.repository.YweetRepository
 import com.dmm.bootcamp.yatter2025.ui.timeline.bindingmodel.converter.YweetConverter
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 
 // StateFlowは保持している値がImmutableになり、初期化以降にデータの更新ができないため、更新が可能なMutableStateFlowと併せて利用します。
@@ -32,6 +34,15 @@ class PublicTimelineViewModel (
             it.copy(
                 yweetList = YweetConverter.convertToBindingModel(yweetList),
             )
+        }
+    }
+
+    // 今回の画面では画面を表示するたびに最新のリストを取得しておきたい関数
+    fun onResume() {
+        viewModelScope.launch { // ViewModelのライフサイクルに合わせたスコープのcoroutine起動
+            _uiState.update { it.copy(isLoading = true) } // UiStateをローディング状態にする
+            fetchPublicTimeline() // fetchPublicTimeline()メソッドを呼び出しYweet一覧を更新
+            _uiState.update { it.copy(isLoading = false) } // UiStateのローディング状態を解除する
         }
     }
 }
