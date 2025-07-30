bdf6cb9529bb37669d2ea9b44271489b147dda18
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2025-07-30 17:16:22 +0900

    UiStateの実装完

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/login/LoginUiState.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/login/LoginUiState.kt
@@ -4,5 +4,23 @@ import com.dmm.bootcamp.yatter2025.ui.login.bindingmodel.LoginBindingModel
 
 data class LoginUiState(
     val loginBindingModel: LoginBindingModel,
-    val isLoading: Boolean,
-)
+    val isLoading: Boolean, //ローディング
+    val validUsername: Boolean, //ゆーざーねーむ入力
+    val validPassword: Boolean, //パスワード入力されたか
+){
+    // ユーザーフレンドリー
+    // ログイン実行可能かのフラグ
+    val isEnableLogin: Boolean = validUsername && validPassword
+
+
+    fun empty(): LoginUiState = LoginUiState(
+        // LoginBindingModelの定義を使う
+        loginBindingModel = LoginBindingModel(
+            username = "",
+            password = ""
+        ),
+        isLoading = false,
+        validUsername = false,
+        validPassword = false,
+    )
+}
