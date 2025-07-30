6e9a1b823ff8a022107d3b287eafdce9b4d32130
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2025-07-30 12:10:28 +0900

    ActivityとPageコンポーザブルとの繋ぎこみ

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/MainActivity.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.dmm.bootcamp.yatter2025
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Surface
+import com.dmm.bootcamp.yatter2025.ui.theme.Yatter2025Theme
+import com.dmm.bootcamp.yatter2025.ui.timeline.PublicTimelinePage
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    // ActivityからJetpack Composeで構築したUIを呼び出すにはsetContent {}を利用
+    setContent {
+      Yatter2025Theme {
+        Surface {
+          PublicTimelinePage()
+        }
+      }
+    }
   }
 }
