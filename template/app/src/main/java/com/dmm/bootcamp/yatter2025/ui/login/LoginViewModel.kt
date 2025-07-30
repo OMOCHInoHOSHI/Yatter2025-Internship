8794a9d2d2dc949593179336819626219f7617d4
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2025-07-30 17:44:03 +0900

    onChenge

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/login/LoginViewModel.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/login/LoginViewModel.kt
@@ -1,11 +1,13 @@
 package com.dmm.bootcamp.yatter2025.ui.login
 
 import androidx.lifecycle.ViewModel
+import com.dmm.bootcamp.yatter2025.domain.model.Password
+import com.dmm.bootcamp.yatter2025.domain.model.Username
 import com.dmm.bootcamp.yatter2025.usecase.login.LoginUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-
+import kotlinx.coroutines.flow.update
 
 
 // ログイン
@@ -20,4 +22,46 @@ class LoginViewModel(
 
     // uiStateは、StateFlowとして公開され、外部から変更できないようにしている
     val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
+
+
+
+    // メソッド定義
+
+    // テキストボックス入力時
+    fun onChangedUsername(username: String) {
+        val snapshotBindingModel = uiState.value.loginBindingModel
+        _uiState.update {
+            it.copy(
+
+                // バリデーション
+                // ドメイン層で実装される
+                validUsername = Username(username).validate(),
+                loginBindingModel = snapshotBindingModel.copy(
+                    username = username
+                )
+            )
+        }
+    }
+
+    fun onChangedPassword(password: String) {
+        val snapshotBindingModel = uiState.value.loginBindingModel
+        _uiState.update {
+            it.copy(
+                // バリデーション
+                validPassword = Password(password).validate(),
+                loginBindingModel = snapshotBindingModel.copy(
+                    password = password
+                )
+            )
+        }
+    }
+
+
+    // ボタン押下時
+    fun onClickLogin() {}
+
+    fun onClickRegister() {}
+
+    fun onCompleteNavigation() {}
+
 }
--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/login/bindingmodel/LoginBindingModelingmodel.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/login/bindingmodel/LoginBindingModelingmodel.kt
@@ -1,5 +1,6 @@
 package com.dmm.bootcamp.yatter2025.ui.login.bindingmodel
 
+// BindingModelは、画面を表示する上で必要な情報をまとめたdata class
 data class LoginBindingModel(
     val username: String,
     val password: String,
