c3310a4c93767344e78f4cb467ca199837d97eea
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2025-07-30 12:03:09 +0900

    PublicTimelinePage

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PublicTimelinePage.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PublicTimelinePage.kt
@@ -1,2 +1,35 @@
 package com.dmm.bootcamp.yatter2025.ui.timeline
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LifecycleEventEffect
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.koin.androidx.compose.getViewModel
+
+
+// ViewModelから状態を取り出し、Templateコンポーザブルに渡す
+@Composable
+fun PublicTimelinePage(
+    //getViewmodel はDMMが定義
+    publicTimelineViewModel: PublicTimelineViewModel = getViewModel(),
+
+    ) {
+
+    // UiStateを抜き出し
+    // publicTimelineViewModel#uiStateの変更を監視する
+    val uiState by publicTimelineViewModel.uiState.collectAsStateWithLifecycle()
+
+    // ライフサイクルイベントで呼び出し
+    // LifecycleEventEffectを使うことによって、指定したライフサイクルのイベントごとに処理を実行
+    LifecycleEventEffect(event = Lifecycle.Event.ON_RESUME) {
+        publicTimelineViewModel.onResume()
+    }
+
+    PublicTimelineTemplate(
+        yweetList = uiState.yweetList,
+        isLoading = uiState.isLoading,
+        isRefreshing = uiState.isRefreshing,
+        onRefresh = publicTimelineViewModel::onRefresh,   // onRefreshはViewModelのメソッドになるため、UiStateからは取得できません は関数オブジェクトとして渡す ::を使う
+    )
+}
