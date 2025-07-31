23fb1e0f2f4f0ff6223803edbfff1e78f1fc3868
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2025-07-31 13:09:22 +0900

    PublicTimelineDestination

---
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/theme/PublicTimelineDestination.kt
@@ -0,0 +1,22 @@
+package com.dmm.bootcamp.yatter2025.ui.theme
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.compose.composable
+import com.dmm.bootcamp.yatter2025.common.navigation.Destination
+import com.dmm.bootcamp.yatter2025.ui.timeline.PublicTimelinePage
+
+// 画面遷移時に必要なパラメータを内包する`Destination`型
+class PublicTimelineDestination(
+    builder: (NavOptionsBuilder.() -> Unit)? = null,
+) : Destination(ROUTE, builder) {
+    companion object {
+        private const val ROUTE = "publicTimeline"
+
+        fun createNode(builder: NavGraphBuilder) {
+            builder.composable(ROUTE) {
+                PublicTimelinePage()
+            }
+        }
+    }
+}
