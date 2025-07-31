7e6bfeab83fb5fd2af3d220fa586a9e0e092620e
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2025-07-31 12:31:50 +0900

    loginpage

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/login/LoginPage.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/login/LoginPage.kt
@@ -1,2 +1,23 @@
 package com.dmm.bootcamp.yatter2025.ui.login
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.koin.androidx.compose.getViewModel
+
+@Composable
+fun LoginPage(
+    loginViewModel: LoginViewModel = getViewModel(),
+) {
+    val uiState: LoginUiState by loginViewModel.uiState.collectAsStateWithLifecycle()
+    LoginTemplate(
+        userName = uiState.loginBindingModel.username,
+        onChangedUserName = loginViewModel::onChangedUsername,  //関数オブジェクトを渡す
+        password = uiState.loginBindingModel.password,
+        onChangedPassword = loginViewModel::onChangedPassword,  //関数オブジェクトを渡す
+        isEnableLogin = uiState.isEnableLogin,
+        isLoading = uiState.isLoading,
+        onClickLogin = loginViewModel::onClickLogin,
+        onClickRegister = loginViewModel::onClickRegister,
+    )
+}
